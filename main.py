6b67ed6a438506c4f593f571bfadef8b914bb484
Frank Schmidt <schmidtfrank190@gmail.com>
2025-03-27 21:41:58 -0500

    main commit

---
+++ main.py
@@ -0,0 +1,14 @@
+#TODO
+#data preprocessing (word stemming? and stop word removal) - im unsure on how stemming will impact the sentiment analysis
+#store lyrics dataset as "hub" data
+#use wordblob? for sentiment analysis on lyrics
+
+
+#future
+#integrate genius api for song searching based on our "hub"
+#things
+
+
+
+
+
