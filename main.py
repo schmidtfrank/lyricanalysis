43ea17ecfd2dff3c2371be3e1f4dbd55cc85b2cb
Frank Schmidt <schmidtfrank190@gmail.com>
2025-03-27 23:42:32 -0500

    preprocess function complete

--- main.py
+++ main.py
@@ -1,14 +1,62 @@
+import pandas as pd
+import re
+import unicodedata
+import nltk
+from nltk.corpus import stopwords
 #TODO
-#data preprocessing (word stemming? and stop word removal) - im unsure on how stemming will impact the sentiment analysis
-#store lyrics dataset as "hub" data
+#word stemming
 #use wordblob? for sentiment analysis on lyrics
 
 
-#future
+#future?
 #integrate genius api for song searching based on our "hub"
 #things
 
 
+#class to contain data
+class LyricSoftware:
+    #initialize internal dataset in the constructor
+    def __init__(self):
+        self.df = pd.read_csv("Songs.csv")
+        pass
+
+    #TODO word stemming
+    def stem(self):
+        pass
+
+    def normalizeText(self,text):
+        #handle unicode
+        return "".join(c for c in unicodedata.normalize("NFKC", text))
+    
+    #preprocess
+    def preprocess(self):
+        replaceDict = {"EmbedShare URLCopyEmbedCopy" : "", "\n" : " "}
+
+        punctuation = "1234567890.,?!;:-'\"*()[]"
+        pattern = f"[{re.escape(punctuation)}]"
+        stop_words = stopwords.words('english')
+
+        for i in self.df.index:
+            #first regex remove punctuation chars
+            self.df.iat[i,2] = self.normalizeText(self.df.iat[i,2])
+            self.df.iat[i,2] = re.sub(pattern, "",self.df.iat[i,2])
+            for key, value in replaceDict.items():
+                #now remove bigger phrase and newlines
+                self.df.iat[i,2] = self.df.iat[i,2].replace(key,value)
+            #lowercase
+            self.df.iat[i,2] = self.df.iat[i,2].lower()
+
+            for word in stop_words:
+                #remove stop words
+                self.df.iat[i,2] = re.sub(r"\b" + re.escape(word) + r"\b", "", self.df.iat[i,2])
+            
+            #finally remove all trailing/duplicate white space
+            self.df.iat[i,2] = re.sub(r"\s+", " ", self.df.iat[i,2]).strip()
 
 
 
+
+l = LyricSoftware()
+l.preprocess()
+
+print(l.df.iat[1,2])
